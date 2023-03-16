85c5d0693a8769016497c1eea32b48022de74215
Marcos Moura <marcosmoura201@hotmail.com>
2023-03-16 10:51:39 -0300

    Implementando o axios e fazendo um get nas informações do back

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
+import axios from "axios"
+import { useEffect, useState } from "react"
+import {Link} from 'react-router-dom'
+import './Home.css'
 
 const Home = () => {
+
+  const [post, setPost] = useState([])
+
+  const getPost = async () => {
+
+    try {
+      
+      const response = await axios.get("http://localhost:8080/food");
+      const data = response.data;
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() =>{
+
+    getPost()
+  },[])
+
+
   return (
     <div>Home</div>
   )
